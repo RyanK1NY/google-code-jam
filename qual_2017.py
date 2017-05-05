62a6bd7cc1a0f842aa8954deb1a41ffa75cd98de
RyanK1NY <ryank1ny@gmail.com>
2017-05-04 20:38:27 -0400

    Read from input, comments to possible solution/determine workflow

--- qual_2017.py
+++ qual_2017.py
@@ -1,4 +1,20 @@
 # Google Code Jam 2017 Qualification Round
 # https://code.google.com/codejam/contest/3264486/dashboard
-# Python 2
+# Python 3
+'''
+- Make pancake into list of chars
+- for every sequence of consecutive characters of length k, flip from "-" to "+"
+- determine maximum number of flips possible, make that the maximum number of attempts (length of pancakes - k possibly?)
 
+'''
+
+
+f = open('A-small-practice.in', 'r')
+# Number of testcases
+t = int(f.readline())
+
+
+for item in range(t):
+    pancake, k = f.readline().split(" ")
+    print(pancake)
+    print(k)
