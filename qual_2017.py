ed6841e739f417ea39269cbdd87450a0c313ef93
RyanK1NY <ryank1ny@gmail.com>
2017-05-06 10:19:04 -0400

    Create list of sublists

--- qual_2017.py
+++ qual_2017.py
@@ -8,13 +8,28 @@
 
 '''
 
+def flip_element(element):
+    if element == "+":
+        element = "-"
+    else:
+        element = "+"
+    return element
+
 
 f = open('A-small-practice.in', 'r')
 # Number of testcases
 t = int(f.readline())
 
-
-for item in range(t):
+for item in range(1):
+    # k is substring length
     pancake, k = f.readline().split(" ")
+    k = int(k)
+    pancake_list = [i for i in pancake]
+    combo_list = []
     print(pancake)
     print(k)
+    for index, value in enumerate(pancake_list):
+        temp_list = pancake_list[index:index+k]
+        if len(temp_list) == k:
+            combo_list.append(temp_list)
+    print(combo_list)
